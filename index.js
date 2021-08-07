6cd444c6216b945d265a3d63e6d49e56ef0bce43
raul152 <48693258+raul152@users.noreply.github.com>
2021-08-07 00:05:10 -0300

    Adicionado algumas mensagens embed.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const ytdl = require('ytdl-core');
 //const token = configs.TOKEN_DISCORD;
 const prefixo = configs.PREFIX;
 //const ytdloptions = {filter: 'audioonly'}
+const ImageUrl = configs.IMAGEURL;
 const servidores = {
     'server': {
         connection: null,
@@ -24,6 +25,7 @@ const servidores = {
 
 //sessão pesquisa youtube
 
+    
 
 botteste.on('message', async msg => {
 
@@ -31,6 +33,12 @@ botteste.on('message', async msg => {
     if(!msg.guild) return;
 
     if(!msg.content.startsWith(prefixo)) return;
+
+    const embed = new Discord.MessageEmbed()
+            .setColor([216,51,255])
+            .setDescription('TOCA DO GABIRU')
+            .setImage(ImageUrl)
+    msg.channel.send(embed);
 
     if(!msg.member.voice.channel)
     {
@@ -108,10 +116,17 @@ botteste.on('message', async msg => {
                 if(resultado)
                 {
                     const id = resultado.data.items[0].id.videoId;
+                    const nomemusica = resultado.data.items[0].snippet.title;
                     tocarmusica = 'https://www.youtube.com/watch?v=' + id;
                     servidores.server.fila.push(tocarmusica);
                     console.log('Adicionado: '+ tocarmusica);
                     musicplayer();
+                    const embed = new Discord.MessageEmbed()
+                        .setColor([216,51,255])
+                        .setTitle(nomemusica)
+                        msg.channel.send(embed);
+
+                    
                 }
             });
         }
@@ -132,6 +147,11 @@ botteste.on('message', async msg => {
 
     }
 
+    //if(msg.content.startsWith(prefixo + 'loop'))
+    //{
+    //    let tocarmusica = msg.content.slice(6);
+    //    servidores.server.dispatcher
+    //}
     //skip    
     if(msg.content === prefixo + 'skip')
     {
@@ -167,4 +187,5 @@ musicplayer = () => {
 botteste.login(configs.TOKEN_DISCORD)
 botteste.on('ready', () => {
     console.log('estou pronto')
+    
 })
